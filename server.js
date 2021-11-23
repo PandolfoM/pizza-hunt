25a884ac6a83502629e1c6bca7c371ccdd4dd5ee
Matthew Pandolfo <matt@pandolfo.com>
2021-11-23 14:34:34 -0500

    find new and modify remove

--- server.js
+++ server.js
@@ -7,10 +7,8 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
-app.use(require('./routes'));
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1/pizza-hunt', {
-  useFindAndModify: false,
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/pizza-hunt', {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
@@ -18,4 +16,6 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1/pizza-hunt', {
 // Use this to log mongo queries being executed!
 mongoose.set('debug', true);
 
-app.listen(PORT, () => console.log(`🌍 Connected on http://localhost:${PORT}/`));
+app.use(require('./routes'));
+
+app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
