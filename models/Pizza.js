e9bf128bfca6da718d61be64eae667328462aafd
Matthew Pandolfo <matt@pandolfo.com>
2021-11-23 14:06:00 -0500

    validate responses

--- models/Pizza.js
+++ models/Pizza.js
@@ -5,17 +5,23 @@ const PizzaSchema = new Schema(
   {
     pizzaName: {
       type: String,
+      required: true,
+      trim: true,
     },
     createdBy: {
       type: String,
     },
     createdAt: {
       type: Date,
+      required: true,
+      trim: true,
       default: Date.now,
       get: (createdAtVal) => dateFormat(createdAtVal),
     },
     size: {
       type: String,
+      required: true,
+      enum: ['Personal', 'Small', 'Medium', 'Large', 'Extra Large'],
       default: "Large",
     },
     toppings: [],
@@ -37,7 +43,10 @@ const PizzaSchema = new Schema(
 
 // get total count of comments and replies on retrieval
 PizzaSchema.virtual("commentCount").get(function () {
-  return this.comments.reduce((total, comment) => total + comment.replies.length + 1, 0)
+  return this.comments.reduce(
+    (total, comment) => total + comment.replies.length + 1,
+    0
+  );
 });
 
 const Pizza = model("Pizza", PizzaSchema);
